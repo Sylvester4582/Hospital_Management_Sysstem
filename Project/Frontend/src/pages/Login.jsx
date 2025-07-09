4b66d5349fe389a6e0f74bcae34b7e504b1d7d85
sylvester4582 <snehil.sanjog@students.iiit.ac.in>
2025-07-09 16:42:39 +0530

    update 16

--- Project/Frontend/src/pages/Login.jsx
+++ Project/Frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     try {
       const response = await axios.post(
         "http://localhost:4000/api/v1/user/login",
-        { email, password },
+        { email, password, role: "Patient" },
         {
           withCredentials: true, headers: { 'Content-Type': 'application/json' }
         });
